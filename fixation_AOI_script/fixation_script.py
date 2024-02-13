afc8ba40ff3062723aa09dba92ae6c7000d3a08f
SAIL <129990907+sail-nu@users.noreply.github.com>
2024-02-13 14:32:59 -0500

    Create fixation_script.py

---
+++ fixation_AOI_script/fixation_script.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import numpy as np
+import csv
+ 
+
+df_fixation = pd.read_csv("subject14_pinwheel_fixations.csv")
+df_task_aoi = pd.read_csv("subject14_pinwheel_step_unix.csv")
+df_task_aoi = df_task_aoi.drop_duplicates()
+
+fixation_frames = []
+fixation_duration = {}
+
+
+for index, row in df_fixation.iterrows():
+    start_frame_index = row['start_frame_index']
+    end_frame_index = row['end_frame_index']
+    for i in range(int(start_frame_index), int(end_frame_index) + 1):
+        fixation_frames.append(i)
+        fixation_duration.update({i: row['duration']})
+
+merged_fixated_list = []
+
+fixation_duration_result = []
+
+for index, row in df_task_aoi.iterrows():
+    if row['image_id'] in fixation_duration:
+        fixation_duration_result.append(fixation_duration.get(row['image_id']))
+    else:
+        fixation_duration_result.append(0)
+
+    if (int(row['image_id']) not in fixation_frames):
+        merged_fixated_list.append(False)
+    else:
+        merged_fixated_list.append(True)
+
+
+df_task_aoi['fixated'] = merged_fixated_list
+df_task_aoi['fixation_duration'] = fixation_duration_result
+
+# df_task_aoi.rename(columns={'Step Number': 'step_number'})
+if "Unnamed: 0" in df_task_aoi:
+    df_task_aoi.pop("Unnamed: 0")
+
+df_task_aoi = df_task_aoi.drop_duplicates()
+
+df_task_aoi.to_csv("fixation_result.csv", index=False)
